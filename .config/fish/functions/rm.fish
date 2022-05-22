9620b8c44f06e824d6e30cef9f302d933ed7c061
fzn0x <fncolon@pm.me>
2022-05-23 00:00:52 +0700

    [fish-config]: create rm aliases

---
+++ .config/fish/functions/rm.fish
@@ -0,0 +1,18 @@
+# Written on fish v3.4.1
+# rm -r delete files recursively
+alias rmr="rm -r"
+# rm -i ask before delete files (interactive deletion)
+alias rmi="rm -i"
+# rm -ri ask before delete files recursively
+alias rmri="rm -ri"
+# rm -f delete file with force
+alias rmf="rm -f"
+# rm -fi ask before delete file with force
+alias rmfi="rm -fi"
+# rm -fr delete files with force recursively
+alias rmfr="rm -fr"
+# rm -fri ask before delete files with force recursively
+alias rmfri="rm -fri"
+
+# show current version of rm
+# alias rmv="rm -v"
