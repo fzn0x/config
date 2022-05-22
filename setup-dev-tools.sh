1d511f28c04ba3cdafa792ef8afa116721a36f03
Muhammad Fauzan <fncolon@pm.me>
2022-05-23 04:25:32 +0700

    [bash-scripts]: rename to setup dev tools

---
+++ setup-dev-tools.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env/fish
+if command -v brew > /dev/null 2>&1
+  brew -v
+else
+  fish_add_path "/opt/homebrew/bin"
+end
+
+# Refactor of Vim that brings a better plugins system and that is easier to contribute
+brew install neovim
+# JSON viewer
+brew install fx
+# The ultimate `ls`
+brew install exa
--- setup-neovim.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env/fish
-if command -v brew > /dev/null 2>&1
-  brew -v
-else
-  fish_add_path "/opt/homebrew/bin"
-end
-
-brew install neovim
