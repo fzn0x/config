3188a973178307aaecf7a2b4213e34b05ae1bdff
Muhammad Fauzan <fncolon@pm.me>
2022-05-23 05:34:27 +0700

    [bash-scripts]: Check if fish command exist on fish setup script

--- setup-fish-terminal.sh
+++ setup-fish-terminal.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env/fish
+if command -v fish > /dev/null 2>&1
+  fish -v
+else
+  brew install fish
+end
+
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install ays
 omf theme ays
