5eb7f7c2603a7620170338d234fc0b642e0ec30f
fzn0x <fncolon@pm.me>
2022-05-23 01:53:23 +0700

    [bash-scripts]: Use fish-shell compatible script

--- setup-neovim.sh
+++ setup-neovim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env/fish
-if command -v brew > /dev/null 2>&1; then
+if command -v brew > /dev/null 2>&1
   brew -v
 else
   fish_add_path "/opt/homebrew/bin"
-fi
+end
 
 brew install neovim
